6d4c23a08f261b8656e87084b9af051e260266d9
ruben18 <ruc219@lehigh.edu>
2015-08-31 20:49:09 +0000

    update WelcomeClass

---
+++ hw-01/WelcomeClass.java
@@ -0,0 +1,20 @@
+// Ruben Castro Espinoza 
+// August 30, 2015
+// CSE2 
+
+public class WelcomeClass{ 
+    
+    // add main method 
+ public static void main(String[]args ){
+     
+ System.out.println ("-------------");
+ System.out.println ("|  WELCOME  |");
+ System.out.println ("-------------");
+ System.out.println (" ^  ^  ^  ^  ^  ^ ");
+ System.out.println  ("/ \\/ \\/ \\/ \\/ \\/ \\");
+ System.out.println ("<-R--U--C--2--1--9->");
+ System.out.println ("\\ /\\ /\\ /\\ /\\ /\\ / ");
+ System.out.println (" v  v  v  v  v  v    ");
+ System.out.println (" Hello I'm Ruben and I am From lancaster California.");
+  }
+}
